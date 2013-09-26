a7b200a263560412b9c08d113781e98511fb4923
howie_yu <howie.yu@gmail.com>
2013-09-26 13:47:35 +0800

    Revert "After write to file need close outputstream"
    This reverts commit 9ca8d7782c731dda67fef1419c27b82e7e80617b.

--- src/main/java/sec/util/Crypto.java
+++ src/main/java/sec/util/Crypto.java
@@ -3,13 +3,16 @@ package sec.util;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.MessageDigest;
-import java.security.spec.AlgorithmParameterSpec;
+
 
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -18,191 +21,169 @@ import javax.crypto.spec.SecretKeySpec;
  * 
  * @author someone
  * @modify geisbruch
- * 
+ *
  */
 public class Crypto {
-	Cipher ecipher;
+    Cipher ecipher;
+    Cipher dcipher;
 
-	Cipher dcipher;
 
-	/**
-	 * Input a string that will be md5 hashed to create the key.
-	 * 
-	 * @return void, cipher initialized
-	 */
+    /**
+    * Input a string that will be md5 hashed to create the key.
+    * @return void, cipher initialized
+    */
+    
+    public Crypto(String key){
+        SecretKeySpec skey = new SecretKeySpec(getMD5(key), "AES");
+        this.setupCrypto(skey);
+    }
 
-	public Crypto(String key) {
-		SecretKeySpec skey = new SecretKeySpec(getMD5(key), "AES");
-		this.setupCrypto(skey);
-	}
+    private void setupCrypto(SecretKey key){
+        // Create an 8-byte initialization vector
+        byte[] iv = new byte[]
+        {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+        };
 
-	private void setupCrypto(SecretKey key) {
-		// Create an 8-byte initialization vector
-		byte[] iv = new byte[]{	0x00,
-								0x01,
-								0x02,
-								0x03,
-								0x04,
-								0x05,
-								0x06,
-								0x07,
-								0x08,
-								0x09,
-								0x0a,
-								0x0b,
-								0x0c,
-								0x0d,
-								0x0e,
-								0x0f};
+        AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
+        try
+        {
+            ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
-		AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
-		try {
-			ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            // CBC requires an initialization vector
+            ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+            dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-			// CBC requires an initialization vector
-			ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-			dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    // Buffer used to transport the bytes from one stream to another
+    byte[] buf = new byte[1024];
 
-	// Buffer used to transport the bytes from one stream to another
-	byte[] buf = new byte[1024];
+    public void encrypt(InputStream in, OutputStream out){
+        try {
+            // Bytes written to out will be encrypted
+            out = new CipherOutputStream(out, ecipher);
 
-	public void encrypt(InputStream in, OutputStream out) {
-		try {
-			// Bytes written to out will be encrypted
-			out = new CipherOutputStream(out, ecipher);
+            // Read in the cleartext bytes and write to out to encrypt
+            int numRead = 0;
+            while ((numRead = in.read(buf)) >= 0){
+                out.write(buf, 0, numRead);
+            }
+        }
+        catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
 
-			// Read in the cleartext bytes and write to out to encrypt
-			int numRead = 0;
-			while((numRead = in.read(buf)) >= 0) {
-				out.write(buf, 0, numRead);
-			}
-			out.close();
-		}
-		catch(java.io.IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+    * Input is a string to encrypt.
+    * @return a Hex string of the byte array
+    */
+    public String encrypt(String plaintext){
+        try{
+            byte[] ciphertext = ecipher.doFinal(plaintext.getBytes("UTF-8"));
+            return this.byteToHex(ciphertext);
+        } catch (Exception e){
+             e.printStackTrace();
+            return null;
+        }
 
-	/**
-	 * Input is a string to encrypt.
-	 * 
-	 * @return a Hex string of the byte array
-	 */
-	public String encrypt(String plaintext) {
-		try {
-			byte[] ciphertext = ecipher.doFinal(plaintext.getBytes("UTF-8"));
-			return byteToHex(ciphertext);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+    }
+    
+    /**
+     * Input is a string to encrypt.
+     * @return a Hex string of the byte array
+     */
+     public byte[] encrypt(byte[] plainBytes){
+         try{
+             byte[] ciphertext = ecipher.doFinal(plainBytes);
+             return ciphertext;
+         } catch (Exception e){
+              e.printStackTrace();
+             return null;
+         }
 
-	}
+     }
 
-	/**
-	 * Input is a string to encrypt.
-	 * 
-	 * @return a Hex string of the byte array
-	 */
-	public byte[] encrypt(byte[] plainBytes) {
-		try {
-			byte[] ciphertext = ecipher.doFinal(plainBytes);
-			return ciphertext;
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+    public void decrypt(InputStream in, OutputStream out){
+        try {
+            // Bytes read from in will be decrypted
+            in = new CipherInputStream(in, dcipher);
 
-	}
+            // Read in the decrypted bytes and write the cleartext to out
+            int numRead = 0;
+            while ((numRead = in.read(buf)) >= 0) {
+                out.write(buf, 0, numRead);
+            }
+        } catch (java.io.IOException e) {
+             e.printStackTrace();
+        }
+    }
 
-	public void decrypt(InputStream in, OutputStream out) {
-		try {
-			// Bytes read from in will be decrypted
-			in = new CipherInputStream(in, dcipher);
+    /**
+    * Input encrypted String represented in HEX
+    * @return a string decrypted in plain text
+    */
+    public String decrypt(String hexCipherText){
+        try{
+            String plaintext = new String(dcipher.doFinal(this.hexToByte(hexCipherText)), "UTF-8");
+            return  plaintext;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-			// Read in the decrypted bytes and write the cleartext to out
-			int numRead = 0;
-			while((numRead = in.read(buf)) >= 0) {
-				out.write(buf, 0, numRead);
-			}
-			out.close();
-		}
-		catch(java.io.IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public byte[] decrypt(byte[] ciphertext){
+        try{
+            return  dcipher.doFinal(ciphertext);
+        } catch (Exception e){
+        	System.out.println("Lenght: "+ ciphertext.length);
+        	for(byte b  : ciphertext){
+        		System.out.print(b);
+        		System.out.print(",");
+        	}
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	/**
-	 * Input encrypted String represented in HEX
-	 * 
-	 * @return a string decrypted in plain text
-	 */
-	public String decrypt(String hexCipherText) {
-		try {
-			String plaintext = new String(dcipher.doFinal(hexToByte(hexCipherText)), "UTF-8");
-			return plaintext;
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    private static byte[] getMD5(String input){
+        try{
+            byte[] bytesOfMessage = input.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return md.digest(bytesOfMessage);
+        }  catch (Exception e){
+             return null;
+        }
+    }
 
-	public byte[] decrypt(byte[] ciphertext) {
-		try {
-			return dcipher.doFinal(ciphertext);
-		}
-		catch(Exception e) {
-			System.out.println("Lenght: " + ciphertext.length);
-			for(byte b : ciphertext) {
-				System.out.print(b);
-				System.out.print(",");
-			}
-			e.printStackTrace();
-			return null;
-		}
-	}
+    static final String HEXES = "0123456789ABCDEF";
 
-	private static byte[] getMD5(String input) {
-		try {
-			byte[] bytesOfMessage = input.getBytes("UTF-8");
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			return md.digest(bytesOfMessage);
-		}
-		catch(Exception e) {
-			return null;
-		}
-	}
+    public static String byteToHex( byte [] raw ) {
+        if ( raw == null ) {
+          return null;
+        }
+        final StringBuilder hex = new StringBuilder( 2 * raw.length );
+        for ( final byte b : raw ) {
+          hex.append(HEXES.charAt((b & 0xF0) >> 4))
+             .append(HEXES.charAt((b & 0x0F)));
+        }
+        return hex.toString();
+    }
 
-	static final String HEXES = "0123456789ABCDEF";
-
-	public static String byteToHex(byte[] raw) {
-		if(raw == null) {
-			return null;
-		}
-		final StringBuilder hex = new StringBuilder(2 * raw.length);
-		for(final byte b : raw) {
-			hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
-		}
-		return hex.toString();
-	}
-
-	public static byte[] hexToByte(String hexString) {
-		int len = hexString.length();
-		byte[] ba = new byte[len / 2];
-		for(int i = 0; i < len; i += 2) {
-			ba[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4) + Character.digit(	hexString.charAt(i + 1),
-																									16));
-		}
-		return ba;
-	}
+    public static byte[] hexToByte( String hexString){
+        int len = hexString.length();
+        byte[] ba = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            ba[i/2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4) + Character.digit(hexString.charAt(i+1), 16));
+        }
+        return ba;
+    }
 
 }
